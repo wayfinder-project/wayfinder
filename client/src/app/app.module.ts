8c38519a51023f2c838f9029a4db79c01ea64eeb
Arthur Panlilio <PanlilioArthur@yahoo.com>
2018-09-06 15:16:15 -0400

    added mapview

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { MapComponent } from './components/map/map.component';
+
+import { FormsModule } from '@angular/forms';
+import { AgmCoreModule, GoogleMapsAPIWrapper } from '@agm/core';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    MapComponent
   ],
   imports: [
-    BrowserModule
+    AgmCoreModule.forRoot({apiKey: 'AIzaSyBrZkV0T-ZJncle1r0SqiwQ2MJB6Qxz3mU'}),
+    BrowserModule,
+    FormsModule,
+    NgbModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    GoogleMapsAPIWrapper 
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
