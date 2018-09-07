a0161e6630634dbd8de9c89a3faf5da880ae88e7
Logan Smith <lorgansmith@gmail.com>
2018-09-07 16:18:16 -0400

    Adding basic places finding.

--- client/src/app/components/map/map.component.ts
+++ client/src/app/components/map/map.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, ViewChild, NgZone, OnInit } from '@angular/core';
-import { MapsAPILoader, AgmMap } from '@agm/core';
+import { MapsAPILoader, AgmMap, LatLngBounds, LatLngBoundsLiteral } from '@agm/core';
 import { GoogleMapsAPIWrapper } from '@agm/core/services';
+import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
 
 declare var google: any;
 
@@ -14,26 +17,27 @@ declare var google: any;
 })
 export class MapComponent implements OnInit {
 
-public lat: Number = 24.799448;
-public lng: Number = 120.979021;
-public origin: any;
-public destination: any;
+  public lat: Number = 24.799448;
+  public lng: Number = 120.979021;
+  public origin: any;
+  public destination: any;
   geocoder: any;
 
   public renderOptions = {
     suppressMarkers: true,
     draggable: true,
     visible: true
-};
+  };
 
 
-public waypoints: any = [];
+  public waypoints: any = [];
 
 
 
   @ViewChild(AgmMap) map: AgmMap;
+  controlmap;
 
-  constructor() {
+  constructor(private http: HttpClient) {
 
   }
 
@@ -47,10 +51,10 @@ public waypoints: any = [];
     this.getDirection();
   }
   getDirection() {
-
-        this.origin = { lat: this.origin.lat, lng: this.origin.lng};
-        this.destination = { lat: this.destination.lat, lng: this.destination.lng};
-        this.map.triggerResize();
+    //console.log(this);
+    this.origin = { lat: this.origin.lat, lng: this.origin.lng };
+    this.destination = { lat: this.destination.lat, lng: this.destination.lng };
+    this.map.triggerResize();
   }
 
   markerDragEnd(m: any, origin: boolean) {
@@ -61,9 +65,76 @@ public waypoints: any = [];
       this.destination.lat = m.coords.lat;
       this.destination.lng = m.coords.lng;
     }
+    this.getPlaces.bind(this)();
     this.getDirection();
-   }
+  }
 
+  getPlaces() {
+    var pyrmont = new google.maps.LatLng(-33.8665433, 151.1956316);
+
+    // var nmap = new google.maps.Map(document.getElementById('map'), {
+    //    center: this.origin,
+    //    zoom: 15
+    //  });
+
+    var request = {
+      location: this.destination,
+      radius: '500',
+      types: ['lodging']
+    };
+
+    console.log(this.controlmap);
+
+    var service = new google.maps.places.PlacesService(this.controlmap);
+    service.nearbySearch(request, this.callback.bind(this));
+  }
+
+ callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+      this.createMarkers.bind(this)(results);
+      console.log(results);
+  }
+}
+createMarkers(places) {
+  const bounds: LatLngBounds = new google.maps.LatLngBounds();
+  //var placesList = document.getElementById('places');
+
+  for (var i = 0, place; place = places[i]; i++) {
+
+    let image = {
+      url: place.icon,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(25, 25)
+    };
+
+    let marker = new google.maps.Marker({
+      map: this.controlmap,
+      icon: image,
+      title: place.name,
+      position: place.geometry.location
+    });
+    bounds.extend(place.geometry.location);
+  }
+  this.controlmap.fitBounds(bounds);
+}
+
+getLatLong(placeid: string, map: any, fn) {
+  let placeService = new google.maps.places.PlacesService(map);
+  placeService.getDetails({
+    placeId: placeid
+  }, function (result, status) {
+    console.log(result.geometry.location.lat());
+    console.log(result.geometry.location.lng())
+  });
+}
+mapReady($event: any) { 
+  // here $event will be of type google.maps.Map 
+  // and you can put your logic here to get lat lng for marker. I have just put a sample code. You can refactor it the way you want.
+  //this.getLatLong('ChIJN1t_tDeuEmsRUsoyG83frY4', $event, null);
+  this.controlmap = $event;
+}
 
 
 }
