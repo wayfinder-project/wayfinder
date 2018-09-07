90cd7f0dedae3c9521d36a47fb369092b1e1d726
Ian Johnson <ianprime0509@gmail.com>
2018-09-07 19:28:43 -0400

    Add JWT authentication (but no token creation yet)

---
+++ server/src/main/java/com/wayfinder/server/security/SecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package com.wayfinder.server.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
--- server/src/main/java/com/wayfinder/server/security/WebSecurityConfig.java
+++ server/src/main/java/com/wayfinder/server/security/WebSecurityConfig.java
@@ -1,5 +1,24 @@
 package com.wayfinder.server.security;
 
-public class WebSecurityConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	private JwtTokenProvider tokenProvider;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.authorizeRequests().antMatchers("/login").permitAll().anyRequest().authenticated();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.addFilterBefore(new JwtTokenFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class);
+	}
 }
