716b06ad2ee94b64abf66e42ea1b15319eeff5fd
Ian Johnson <ianprime0509@gmail.com>
2018-09-09 16:43:55 -0400

    Connect components through routing and fix access guard

--- client/src/app/app-routing/app-routing.module.ts
+++ client/src/app/app-routing/app-routing.module.ts
@@ -4,6 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from '../components/login/login.component';
 import { HomeComponent } from '../components/home/home.component';
 import { AccessGuard } from '../guards/access.guard';
+import { UserpageComponent } from '../components/userpage/userpage.component';
+import { MapComponent } from '../components/map/map.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -12,20 +14,18 @@ export const routes: Routes = [
     component: HomeComponent,
     data: { requiresLogin: true },
     canActivate: [AccessGuard],
+    children: [
+      { path: 'account', component: UserpageComponent },
+      { path: 'map', component: MapComponent },
+      { path: '', redirectTo: 'account', pathMatch: 'full' },
+    ],
   },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule.forRoot(routes)
-  ],
+  imports: [CommonModule, RouterModule.forRoot(routes)],
   declarations: [],
-  exports: [
-    RouterModule
-  ]
+  exports: [RouterModule],
 })
-
-export class AppRoutingModule { }
-
+export class AppRoutingModule {}
