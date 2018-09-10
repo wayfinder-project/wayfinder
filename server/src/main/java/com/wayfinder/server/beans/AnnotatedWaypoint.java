2531c5c301f69880e7c501aed363a1765eec5a3c
Ian Johnson <ianprime0509@gmail.com>
2018-09-10 14:39:06 -0400

    Add annotated waypoints to Trip model

---
+++ server/src/main/java/com/wayfinder/server/beans/AnnotatedWaypoint.java
@@ -0,0 +1,44 @@
+package com.wayfinder.server.beans;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("prototype")
+@Entity
+public class AnnotatedWaypoint extends Waypoint {
+	/**
+	 * The custom name that the user gave to this waypoint, if any.
+	 */
+	private String name;
+	/**
+	 * Any comments that the user may have added to the waypoint.
+	 */
+	@OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@NotNull
+	private List<String> comments;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<String> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<String> comments) {
+		this.comments = comments;
+	}
+}
